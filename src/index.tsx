04da72c66db2ce1d469607ade67872f1d804d771
赵越 <zhaoyue11@jd.com>
2021-05-07 14:40:45 +0800

    ✨ add eslint and prettier

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import '@/styles/index.css'
-import "@/styles/tailwind.css"
+import '@/styles/tailwind.css'
 
 const App = () => {
   return <div className="example h-3">hello world</div>
